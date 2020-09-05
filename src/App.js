48a1ac04e6471fc05fc2680e5a67ac84a414808a
Ajeet jha <jhaajeetcs@gmail.com>
2020-09-05 10:06:56 +0530

    side bar implemented

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MiniDrawer from './components/sideBar';
+function App() {
+  return (
+    <div className="App">
+      <MiniDrawer />
+    </div>
+  );
+}
+
+export default App;
